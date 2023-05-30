ba749bef9e67824191da740c5e5e81c7d6007a1b
Murat03 <muratipekk033@gmail.com>
2023-05-30 10:22:47 +0300

    Repository Base Created

---
+++ Repositories/EfCore/Config/BookConfig.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EfCore.Config
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+                new Book { Id = 1, Title = "Satranç", Price = 60 },
+                new Book { Id = 2, Title = "Nasreddin", Price = 20 },
+                new Book { Id = 3, Title = "Utopia", Price = 100 }
+                );
+        }
+    }
+}
--- Repositories/EfCore/RepositoryContext.cs
+++ Repositories/EfCore/RepositoryContext.cs
@@ -1,14 +1,20 @@
 ﻿using Entities.Models;
 using Microsoft.EntityFrameworkCore;
-using WebApi.Repositories.Config;
+using Repositories.EfCore.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace WebApi.Repositories
+namespace Repositories.EfCore
 {
     public class RepositoryContext : DbContext
     {
         public RepositoryContext(DbContextOptions options) : base(options)
         {
-            
+
         }
         public DbSet<Book> Books { get; set; }
 
