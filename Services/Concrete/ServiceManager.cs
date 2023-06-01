13afb92fce9cef797998e2c1ed0fd2fba94c94dd
Murat03 <muratipekk033@gmail.com>
2023-06-01 19:35:30 +0300

    Logger finished

--- Services/Concrete/ServiceManager.cs
+++ Services/Concrete/ServiceManager.cs
@@ -11,9 +11,10 @@ namespace Services.Concrete
     public class ServiceManager : IServiceManager
     {
         private readonly Lazy<IBookService> _bookService;
-        public ServiceManager(IRepositoryManager repositoryManager)
+        public ServiceManager(IRepositoryManager repositoryManager,
+            ILoggerService loggerService)
         {
-            _bookService = new Lazy<IBookService>(() => new BookManager(repositoryManager));
+            _bookService = new Lazy<IBookService>(() => new BookManager(repositoryManager, loggerService));
         }
         public IBookService BookService => _bookService.Value;
     }
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.ConfigureSqlContext(builder.Configuration);
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureLoggerManager();
 
 var app = builder.Build();
 
