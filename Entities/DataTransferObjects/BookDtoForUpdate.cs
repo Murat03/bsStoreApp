e5ec8031da3293c948e6ec152cea898f992e3b98
Murat03 <muratipekk033@gmail.com>
2023-06-02 15:33:10 +0300

    AutoMapper Added

---
+++ Entities/DataTransferObjects/BookDtoForUpdate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.DataTransferObjects
+{
+    public record BookDtoForUpdate(int Id, string Title, decimal Price);
+    /*
+    public record BookDtoForUpdate()
+    {
+        public int Id { get; init; }
+        public string Title { get; init; }
+        public decimal Price { get; init; }
+    }
+    */
+}
--- Services/Concrete/ServiceManager.cs
+++ Services/Concrete/ServiceManager.cs
@@ -1,4 +1,5 @@
-﻿using Repositories.Contracts;
+﻿using AutoMapper;
+using Repositories.Contracts;
 using Services.Contracts;
 using System;
 using System.Collections.Generic;
@@ -12,9 +13,11 @@ namespace Services.Concrete
     {
         private readonly Lazy<IBookService> _bookService;
         public ServiceManager(IRepositoryManager repositoryManager,
-            ILoggerService loggerService)
+            ILoggerService loggerService,
+            IMapper mapper)
         {
-            _bookService = new Lazy<IBookService>(() => new BookManager(repositoryManager, loggerService));
+            _bookService = new Lazy<IBookService>(() => 
+            new BookManager(repositoryManager, loggerService, mapper));
         }
         public IBookService BookService => _bookService.Value;
     }
