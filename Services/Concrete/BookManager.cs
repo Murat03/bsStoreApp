72e47da00f4dc8bed86270d32cdaba1047b36ea9
Murat03 <muratipekk033@gmail.com>
2023-06-02 14:38:10 +0300

    Error Exception Handling

--- Services/Concrete/BookManager.cs
+++ Services/Concrete/BookManager.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Models;
+﻿using Entities.Exceptions;
+using Entities.Models;
 using Repositories.Contracts;
 using Services.Contracts;
 using System;
@@ -32,11 +33,7 @@ namespace Services.Concrete
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
 
             if (entity is null)
-            {
-                string message = $"The book with id:{id} could not found.";
-                _logger.LogInfo(message);
-                throw new Exception(message);
-            }
+                throw new BookNotFoundException(id);
 
             _manager.Book.DeleteOneBook(entity);
             _manager.Save();
@@ -54,7 +51,7 @@ namespace Services.Concrete
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
 
             if (entity is null)
-                throw new Exception($"Book with id:{id} could not found.");
+                throw new BookNotFoundException(id);
 
             return entity;
         }
@@ -64,15 +61,8 @@ namespace Services.Concrete
             var entity = _manager.Book.GetOneBookById(id, trackChanges);
 
             if (entity is null)
-            {
-                string message = $"The book with id:{id} could not found.";
-                _logger.LogInfo(message);
-                throw new Exception(message);
-            }
+                throw new BookNotFoundException(id);
 
-            if (book is null)
-                throw new ArgumentNullException(nameof(book));
-            
             entity.Title = book.Title;
             entity.Price = book.Price;
 
